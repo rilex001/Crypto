7c59e7692c7674f79410e9f6f31fd3d91fcc3216
rilex001 <ristic1900@gmail.com>
2020-11-01 00:26:08 +0100

    fetch data

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,16 +1,30 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Information = () => {
+
+    const [data, setData] = useState([])
 
     useEffect(() => {
         fetch("https://api.coingecko.com/api/v3/coins/list")
             .then(result => result.json())
-            .then(data => console.log(data))
+            .then(data => setData(data))
             .catch(err => console.log(err))
     }, [])
+
+
     return (
+
+        
         <div>
-            
+            {
+               data.length ? data.map(crypto => 
+                <div key={crypto.id} className='cryptoinf'>
+                    <p>{crypto.symbol}</p>
+                    <p>{crypto.name}</p>
+                </div>
+            ) : <div>Wait</div>
+                
+            }
         </div>
     )
 }
